02e88dabb1335cc313bd3c4d7e1e968d524ea4bf
muhammadziyo-web <mrvaliyev2006@gmail.com>
2022-08-17 01:57:04 +0500

    JS MISOLLAR

---
+++ script.js
@@ -0,0 +1,159 @@
+// // ================1================
+// for (let k = 1; k <= 6; k++) {
+// console.log(1);
+// }
+
+
+
+// // ================2================
+// for (let a = 1; a<= 9; a++) {
+// console.log(a);
+// }
+
+
+
+// // ================3================
+// for (let a= 10; a> 2; a--) {
+//     if(a==10) continue
+// console.log(a);
+// }
+
+
+// // ================4================
+// for (let i = 1; i <=10; i++) {
+//     console.log(`${i}kg konfet ${i*10000} so'm`);
+// }
+
+
+// // ================5================
+// for (let i = 1; i <=10; i++) {
+//     console.log(`${i/10}kg konfet ${i*1000} so'm`);
+// }
+
+// // ================6================
+// for (let i = 10; i <=20; i++) {
+//     if(i%2 >0) continue
+//     console.log(`${i/10}kg konfet ${i*1000} so'm`);
+// }
+
+
+
+// // ================7================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 0;
+// for (let i=num1 ; i<=num ; i++){
+//     sum+=i
+// }
+// alert(sum);
+
+
+// // ================8================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 1;
+// for (let i=num1 ; i<=num ; i++){
+//     sum*=i
+// }
+// alert(sum);
+
+
+// // ================9================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 0;
+// for (let i=num1 ; i<=num ; i++){ 
+//     sum= sum + (i*i)
+// }
+// alert(sum);
+
+
+// // ================10================
+// let n = 2 ;
+// let sum = 0;
+// for(let i=1 ; i<=n ; i++){
+// sum = sum + (1/i)
+// }
+// console.log(sum);
+
+// // ================11================
+// let num1 = 1;
+// let num2 = 20;
+// for(let i=num1; i<=num2; i++){
+//     if(i%2==0) continue
+//     console.log(i);
+// }
+
+
+// // ================12================
+// let num1 = 1;
+// let num2 = 20;
+// for(let i=num1; i<=num2; i++){
+//     if(i%2>0) continue
+//     console.log(i);
+// }
+
+
+
+
+// // ================13================
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//     let count = 0;
+//     for (let j = 1; j <= i; j++) {
+//         if (i%j == 0)
+//             count++
+//     }
+//     if (count==2) {
+//         } else {
+//             console.log(i)
+//         }
+// }
+
+
+
+// // ================14================
+// let n = 10;
+// for(let i=1 ; i<=n; i++){
+//     console.log(i*i);
+// }
+
+
+// // ================15================
+// let n=24;
+// for(let i=n; i>=1; i--){
+//     console.log(i);
+// }
+
+
+
+
+// // ================16================
+// let n =100;
+// for(let i=1; i<=100; i++){
+//     if(i%5==0)
+//     console.log(i);
+// }
+
+
+// // ================17================
+// let n = 5,
+//     result = 1;
+// for(let i=1; i<=n; i++){
+//     result*=i;
+// }
+// console.log(result);
+
+
+// // ================18================
+// let n = 3,
+//     result = 1,
+//     sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++){
+//         result*=j
+        
+//     }
+// }
+// console.log(result);
+
